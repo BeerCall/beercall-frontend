727ed445c78fe5857ac22262db4b8d891548893b
migz3rrr <miguel.joubert@epitech.eu>
2026-03-22 15:30:28 +0100

    fix(config): re-add svg

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         basicSsl(), // 🔒 Parfait pour tester la PWA sur ton tel en HTTPS !
         VitePWA({
             registerType: 'autoUpdate',
-            includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+            includeAssets: ['favicon.svg', 'apple-touch-icon.svg'],
             manifest: {
                 name: 'BeerCall',
                 short_name: 'BeerCall',
@@ -22,14 +22,14 @@ export default defineConfig({
                 orientation: 'portrait',
                 icons: [
                     {
-                        src: '/pwa-192x192.png',
+                        src: '/pwa-192x192.svg',
                         sizes: '192x192',
-                        type: 'image/png'
+                        type: 'image/svg'
                     },
                     {
-                        src: '/pwa-512x512.png',
+                        src: '/pwa-512x512.svg',
                         sizes: '512x512',
-                        type: 'image/png',
+                        type: 'image/svg',
                         purpose: 'any maskable'
                     }
                 ]
