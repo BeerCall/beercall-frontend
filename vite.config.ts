f21a16cd4e1517b307281275f87cf22e1fc2069e
migz3rrr <miguel.joubert@epitech.eu>
2026-03-22 15:18:00 +0100

    feat: put in place pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,38 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
         react(),
-        tailwindcss(),
-        basicSsl()
+        VitePWA({
+            registerType: 'autoUpdate', // Met à jour l'appli en arrière-plan chez tes potes
+            includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'BeerCall',
+                short_name: 'BeerCall',
+                description: 'Ne rate plus aucun apéro avec ta Squad !',
+                theme_color: '#f8fafc', // Correspond au bg de ton app
+                background_color: '#f8fafc',
+                display: 'standalone', // Cache le navigateur (UI 100% Native)
+                orientation: 'portrait', // Bloque la rotation
+                icons: [
+                    {
+                        src: '/pwa-192x192.svg',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/pwa-512x512.svg',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable' // Android pourra arrondir le logo proprement
+                    }
+                ]
+            }
+        })
     ],
     server: {
         host: true,
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-                secure: false,
-            },
-            '/models': {
-                target: 'http://127.0.0.1:8000', // Remplace par l'IP/Port de ton backend
-                changeOrigin: true,
-            }
-        }
     }
-})
+});
